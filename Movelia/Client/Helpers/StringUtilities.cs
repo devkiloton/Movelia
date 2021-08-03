089ee3fc394f9869c9b5528ea746e9ff2d72876c
devkiloton <dev.kiloton@gmail.com>
2021-08-03 19:21:51 -0300

    The beginning

---
+++ Movelia/Client/Helpers/StringUtilities.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Movelia.Client.Helpers
+{
+    public class StringUtilities
+    {
+        public static string UpperName(string name)
+        {
+            return name = name.ToUpper();
+        }
+    }
+
+}
---
+++ Movelia/Shared/Entities/Movie.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Movelia.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title {get; set;}
+        public DateTime ReleaseDate {get; set;}
+    }
+
+}
